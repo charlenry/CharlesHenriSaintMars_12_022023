8eede4fa52303e18f82cb9761378868ec484c9dc
Charles-Henri St-Mars <ch.stmars@gmail.com>
2023-03-09 20:03:37 +0100

    Remplacement des px par des rem pour certaines propriétés

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import './Home.scss';
+import "./Home.scss";
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -6,18 +6,16 @@ import useBrowserWidth from "../../service/useBrowserWidth";
 import useFetchService from "../../service/useFetchService";
 import UserInfosModel from "../../dataModels/UserInfosModel";
 import ActivityModel from "../../dataModels/ActivityModel";
-import AverageSessionsModel from '../../dataModels/AverageSessionsModel';
-import PerformanceModel from '../../dataModels/PerformanceModel';
+import AverageSessionsModel from "../../dataModels/AverageSessionsModel";
+import PerformanceModel from "../../dataModels/PerformanceModel";
 import Navbar from "../../components/Navbar/Navbar";
 import IconsBar from "../../components/IconsBar/IconsBar";
-import Message from '../../components/Message/Message';
-import Nutrients from '../../components/Nutrients/Nutrients';
-import TodayScore from '../../components/TodayScore/TodayScore';
-import Activity from '../../components/Activity/Activity';
-import AverageSessions from '../../components/AverageSessions/AverageSessions';
-import Performance from '../../components/Performance/Performance';
-
-
+import Message from "../../components/Message/Message";
+import Nutrients from "../../components/Nutrients/Nutrients";
+import TodayScore from "../../components/TodayScore/TodayScore";
+import Activity from "../../components/Activity/Activity";
+import AverageSessions from "../../components/AverageSessions/AverageSessions";
+import Performance from "../../components/Performance/Performance";
 
 const Home = (props) => {
   const { id } = useParams();
@@ -31,7 +29,9 @@ const Home = (props) => {
   const activityModel = new ActivityModel(activity.userActivity);
 
   const averageSessions = useFetchService(id, "averageSessions");
-  const averageSessionsModel = new AverageSessionsModel(averageSessions.userAverageSessions);
+  const averageSessionsModel = new AverageSessionsModel(
+    averageSessions.userAverageSessions
+  );
 
   const performance = useFetchService(id, "performance");
   const performanceModel = new PerformanceModel(performance.userPerformance);
@@ -40,21 +40,32 @@ const Home = (props) => {
 
   const browserWidth = useBrowserWidth();
   const [navbarWidth, setNavbarWidth] = useState();
+  const coeffDirecteur = (62.5 - 47.5) / (1440 - 1024);
 
   useEffect(() => {
-    if (browserWidth < 1457) {
-      setNavbarWidth(browserWidth - 20);
-    } else {
-      setNavbarWidth(1440);
+    // if (browserWidth < 1457) {
+    //   setNavbarWidth(browserWidth - 20);
+    // } else {
+    //   setNavbarWidth(1440);
+    // }
+
+    const html = document.querySelector("html");
+    let actualFontPercent = coeffDirecteur * browserWidth;
+
+    if (browserWidth < 1440) {
+      html.style.fontSize = `${actualFontPercent}%`;
+    } else if (browserWidth >= 1440) {
+      html.style.fontSize = "62.5%";
     }
-  }, [browserWidth])
+
+  }, [browserWidth]);
 
 
   return (
     <>
       <div className="center">
         <div className="container">
-          <Navbar id={infosModel.id} navbarWidth= {navbarWidth} />
+          <Navbar id={infosModel.id} navbarWidth={navbarWidth} />
           <IconsBar />
           <main className="main">
             <Message infosModel={infosModel} />
@@ -64,7 +75,7 @@ const Home = (props) => {
 
               <div className="wrapper-other-charts">
                 <AverageSessions averageSessionsModel={averageSessionsModel} />
-                <Performance performanceModel={performanceModel} /> 
+                <Performance performanceModel={performanceModel} />
                 <TodayScore infosModel={infosModel} />
               </div>
 
